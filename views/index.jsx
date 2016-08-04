811278ef5f41ca027f36912004303e475504b5c7
xiayu0321 <xiayugogo0321@163.com>
2016-08-04 22:39:51 +0800

    exer props

--- views/index.jsx
+++ views/index.jsx
@@ -1,36 +1,53 @@
 import React from 'react';
 
 export default class TodoBox extends React.Component {
+    // Omitted
+    render() {
+            return (
+                <div className="todoBox">
+                    <h1>Todos</h1>
+                    <TodoList />
+                    <TodoForm />
+                </div>
+            );
+    }
+}
+
+class TodoList extends React.Component {
     render() {
         return (
-            <div className="todoBox">
-                <h1>Todos</h1>
-                <TodoList />
-                <TodoForm />
+            <div className="todoList">
+                <table style={{border: "2px solid black"}}>
+                    <tbody>
+                    <Todo title="Shopping">Milk</Todo>
+                    <Todo title="Hair cut">13:00</Todo>
+                    </tbody>
+                </table>
             </div>
         );
     }
 }
 
-class TodoList extends React.Component {
+class Todo extends React.Component {
     // Write code here
-    render(){
+    render (){
         return (
-            <div className="todoList">
-                I am a TodoList.
-            </div>
-        )
+            <tr>
+                <td style={{border:"1px solid black"}}>{this.props.title}</td>
+                <td style={{border:"1px solid black"}}>{this.props.children}</td>
+            </tr>
+        );
     }
 }
 
 class TodoForm extends React.Component {
-    // Write code here
-    render(){
+    // Omitted
+    render() {
         return (
             <div className="todoForm">
                 I am a TodoForm.
             </div>
-        )
+        );
     }
-}
 
+}
