f7ae3f0f3fa8b4a086ca262be794fbd6a1251415
xiayu0321 <xiayugogo0321@163.com>
2016-08-06 00:23:06 +0800

    propsformserver

--- program.js
+++ program.js
@@ -10,8 +10,18 @@ require('babel/register')({
     ignore: false
 });
 
+// write below
+var data = [{
+    title:"Shopping",
+    detail:process.argv[3]
+},{
+    title:"Hair cut",
+    detail:process.argv[4]
+}];
+
 app.use('/', function(req, res) {
-    res.render('index', '');
+    res.render('index', {data: data});
 });
 
 app.listen(app.get('port'), function() {});
+
--- views/index.jsx
+++ views/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 export default class TodoBox extends React.Component {
-    // Omitted
-    render(){
+    render() {
         return (
             <div className="todoBox">
                 <h1>Todos</h1>
-                <TodoList />
+                <TodoList data = {this.props.data} />
                 <TodoForm />
             </div>
         );
@@ -14,15 +13,13 @@ export default class TodoBox extends React.Component {
 }
 
 class TodoList extends React.Component {
-    // Omitted
-    render(){
+    render() {
+        var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
         return (
-            <div className="todoList">
-                <table style={{border:"2px solid black"}}>
+            <div className = "todoList">
+                <table style={{border: "2px solid black"}}>
                     <tbody>
-                    <Todo title="Shopping">Milk</Todo>
-                    <Todo title="Hair cut">13:00</Todo>
-                    <Todo title="Learn React">15:00</Todo>
+                    {todo}
                     </tbody>
                 </table>
             </div>
@@ -35,12 +32,9 @@ class Todo extends React.Component {
         super(props);
         this.state=({checked:false});
     }
-
-
     handleChange(e) {
         this.setState({checked: e.target.checked});
     }
-
     render() {
         return (
             <tr>
@@ -53,9 +47,10 @@ class Todo extends React.Component {
         );
     }
 }
-       Todo.propTypes = {
-        title: React.PropTypes.string.isRequired
-}
+Todo.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
+
 class TodoForm extends React.Component {
     // Omitted
     render(){
@@ -68,8 +63,8 @@ class TodoForm extends React.Component {
 }
 
 let style = {
+    // Omitted
     tableContent: {
         border: "1px solid black"
     }
 };
-
